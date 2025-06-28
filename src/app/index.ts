54e4338050363d84ccbeb97eacc840eea23eb21a
Ludvík Prokopec <ludvik.prokopec@gmail.com>
2025-06-28 18:45:52 +0200

    add mine endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,8 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { BlockChain } from '../blockchain/BlockChain.js';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
 const blockChain = new BlockChain();
 
@@ -8,6 +10,14 @@ const hono = new Hono();
 
 hono.get('/blocks', (c) => {
   return c.json(blockChain.getChain());
+});
+
+hono.post('/mine', zValidator('json', z.record(z.unknown())), (c) => {
+  const data = c.req.valid('json');
+
+  const newBlock = blockChain.addBlock(data);
+
+  return c.json(newBlock.toJSON());
 });
 
 const port = 3000;
