1f6756a8533375b89dafcfc731a970a471056c55
lewis-wow <ludvik.prokopec@gmail.com>
2025-07-19 18:51:18 +0200

    add kBucket tests

---
+++ __tests__/unit/kademlia/KBucket.test.ts
@@ -0,0 +1,63 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+import { KBucket } from '../../../src/kademlia/KBucket.js';
+import { Contact } from '../../../src/Contact.js';
+import { K_BUCKET_SIZE } from '../../../src/consts.js';
+
+describe('KBucket', () => {
+  let kBucket: KBucket, contact: Contact, nodeId: string;
+
+  beforeEach(() => {
+    nodeId = 'nodeId0';
+    kBucket = new KBucket();
+    contact = new Contact({
+      nodeId,
+      address: 'address',
+      port: 0,
+    });
+    kBucket.add(contact);
+  });
+
+  test('getContact()', () => {
+    expect(kBucket.getContact(nodeId)).toEqual(contact);
+  });
+
+  test('hasContacts()', () => {
+    expect(kBucket.hasContact(nodeId)).toBe(true);
+  });
+
+  test('getContacts()', () => {
+    expect(kBucket.getContacts()).toEqual([contact]);
+  });
+
+  describe('full bucket', () => {
+    let newContacts: Contact[];
+
+    beforeEach(() => {
+      newContacts = [];
+      for (let i = 1; i < K_BUCKET_SIZE; i++) {
+        const newContact = new Contact({
+          nodeId: `nodeId${i}`,
+          address: 'address',
+          port: 0,
+        });
+        kBucket.add(newContact);
+        newContacts.push(newContact);
+      }
+    });
+
+    test('getContacts()', () => {
+      expect(kBucket.getContacts()).toEqual([contact, ...newContacts]);
+    });
+
+    test('add()', () => {
+      const newContact = new Contact({
+        nodeId: `nodeId${K_BUCKET_SIZE}`,
+        address: 'address',
+        port: 0,
+      });
+      kBucket.add(newContact);
+
+      expect(kBucket.getContacts()).toEqual([contact, ...newContacts]);
+    });
+  });
+});
